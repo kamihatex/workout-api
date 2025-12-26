c667833868cf279f5e9c815491aae809cf51a9e3
kamihatex <plutoismycousin@gmail.com>
2025-12-26 21:11:24 +0200

    variables and loops

--- main.go
+++ main.go
@@ -6,4 +6,29 @@ import (
 
 func main() {
 	fmt.Println("first go program ever :D!")
+
+	// variables
+
+	var name string = "Kami"
+	fmt.Printf("this is my name %s\n", name)
+
+	age := 26
+	fmt.Printf("and this is my age %d\n", age)
+
+	// loops
+
+	for i := 0; i < 5; i++ {
+		fmt.Println("this is i: ", i)
+	}
+
+	counter := 0
+	for counter < 3 {
+		fmt.Println("this is counter: ", counter)
+		counter++
+	}
+
+	// arrays and slices
+
+	numbers := [5]int{1, 2, 3, 4, 5}
+	fmt.Printf("these are our numbers %v\n", numbers)
 }
