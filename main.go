02d1b2f8aed7c0e5d4aa8f1aed209fff01a79786
kamihatex <plutoismycousin@gmail.com>
2025-12-28 17:31:14 +0200

    housing and logging application

--- main.go
+++ main.go
@@ -1,34 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"workoutGo/internal/app"
 )
 
 func main() {
-	fmt.Println("first go program ever :D!")
+	app, err := app.NewApplication()
 
-	// variables
-
-	var name string = "Kami"
-	fmt.Printf("this is my name %s\n", name)
-
-	age := 26
-	fmt.Printf("and this is my age %d\n", age)
-
-	// loops
-
-	for i := 0; i < 5; i++ {
-		fmt.Println("this is i: ", i)
+	if err != nil {
+		panic(err)
 	}
-
-	counter := 0
-	for counter < 3 {
-		fmt.Println("this is counter: ", counter)
-		counter++
-	}
-
-	// arrays and slices
-
-	numbers := [5]int{1, 2, 3, 4, 5}
-	fmt.Printf("these are our numbers %v\n", numbers)
+	app.Logger.Println("app is running...")
 }
