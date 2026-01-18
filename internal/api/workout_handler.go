17da9668231c443cfe6a834428adbc575e452ea6
kamihatex <plutoismycousin@gmail.com>
2026-01-18 20:03:39 +0200

    creating workout handler and adding it to app file

--- internal/api/workout_handler.go
+++ internal/api/workout_handler.go
@@ -1,17 +1,26 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
+	"workoutGo/internal/store"
 
 	"github.com/go-chi/chi/v5"
 )
 
-type WorkoutHandler struct{}
+type WorkoutHandler struct {
+	workoutStore store.WorkoutStore
+	logger       *log.Logger
+}
 
-func NewWorkoutHandler() *WorkoutHandler {
-	return &WorkoutHandler{}
+func NewWorkoutHandler(workoutStore store.WorkoutStore, logger *log.Logger) *WorkoutHandler {
+	return &WorkoutHandler{
+		workoutStore: workoutStore,
+		logger:       logger,
+	}
 }
 
 func (wh *WorkoutHandler) HandleGetWorkoutById(w http.ResponseWriter, r *http.Request) {
@@ -31,5 +40,22 @@ func (wh *WorkoutHandler) HandleGetWorkoutById(w http.ResponseWriter, r *http.Re
 }
 
 func (wh *WorkoutHandler) HandleCreateWorkout(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "created a workout\n")
+	var workout store.Workout
+	err := json.NewDecoder(r.Body).Decode(&workout)
+	if err != nil {
+		wh.logger.Printf("ERROR: decodingCreateWorkout: %v", err)
+		http.Error(w, "failed to create workout", http.StatusInternalServerError)
+		//utils.WriteJSON(w, http.StatusBadRequest, utils.Envelope{"error": "invalid request sent"})
+		return
+	}
+
+	createdWorkout, err := wh.workoutStore.CreateWorkout(&workout)
+	if err != nil {
+		wh.logger.Printf("ERROR: createWorkout: %v", err)
+		http.Error(w, "failed to create workout", http.StatusInternalServerError)
+		//utils.WriteJSON(w, http.StatusInternalServerError, utils.Envelope{"error": "failed to create workout"})
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(createdWorkout)
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -30,9 +30,9 @@ func NewApplication() (*Application, error) {
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 	//the store goes here
-
+	workoutStore := store.NewPostgresWorkoutStore(pgDB)
 	//and handler here
-	workoutHandler := api.NewWorkoutHandler()
+	workoutHandler := api.NewWorkoutHandler(workoutStore, logger)
 
 	app := &Application{
 		Logger:         logger,
