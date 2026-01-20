1d6ac1f03ae42ed5bed549172fb598f868a078bf
kamihatex <plutoismycousin@gmail.com>
2026-01-20 20:13:21 +0200

    making update store and handler functions

--- internal/api/workout_handler.go
+++ internal/api/workout_handler.go
@@ -36,6 +36,18 @@ func (wh *WorkoutHandler) HandleGetWorkoutById(w http.ResponseWriter, r *http.Re
 		return
 	}
 
+	workout, err := wh.workoutStore.GetWorkoutByID(workoutID)
+	if err != nil {
+		wh.logger.Printf("ERROR: decodingCreateWorkout: %v", err)
+		http.Error(w, "failed to fetch the workout", http.StatusInternalServerError)
+		//utils.W
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(workout)
+
 	fmt.Fprintf(w, "this is the workout id %d\n", workoutID)
 }
 
@@ -58,4 +70,94 @@ func (wh *WorkoutHandler) HandleCreateWorkout(w http.ResponseWriter, r *http.Req
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(createdWorkout)
+}
+
+func (wh *WorkoutHandler) HandleUpdateWorkoutByID(w http.ResponseWriter, r *http.Request) {
+	paramsWorkoutID := chi.URLParam(r, "id")
+	if paramsWorkoutID == "" {
+		http.NotFound(w, r)
+		return
+	}
+
+	workoutID, err := strconv.ParseInt(paramsWorkoutID, 10, 64)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	existingWorkout, err := wh.workoutStore.GetWorkoutByID(workoutID)
+	if err != nil {
+		wh.logger.Printf("ERROR: decodingCreateWorkout: %v", err)
+		http.Error(w, "failed to fetch the workout", http.StatusInternalServerError)
+		//utils.W
+		return
+	}
+	// at this point we can assume we are able to find an existing workout
+	var updateWorkoutRequest struct {
+		Title           *string              `json:"title"`
+		Description     *string              `json:"description"`
+		DurationMinutes *int                 `json:"duration_minutes"`
+		CaloriesBurned  *int                 `json:"calories_burned"`
+		Entries         []store.WorkoutEntry `json:"entries"`
+	}
+
+	err = json.NewDecoder(r.Body).Decode(&updateWorkoutRequest)
+	if err != nil {
+		wh.logger.Printf("ERROR: decodingUpdateRequest: %v", err)
+		//utils.WriteJSON(w, http.StatusBadRequest, utils.Envelope{"error": "invalid request payload"})
+		http.Error(w, "failed to create workout", http.StatusInternalServerError)
+		//
+		return
+	}
+
+	if updateWorkoutRequest.Title != nil {
+		existingWorkout.Title = *updateWorkoutRequest.Title
+	}
+	if updateWorkoutRequest.Description != nil {
+		existingWorkout.Description = *updateWorkoutRequest.Description
+	}
+	if updateWorkoutRequest.DurationMinutes != nil {
+		existingWorkout.DurationMinutes = *updateWorkoutRequest.DurationMinutes
+	}
+	if updateWorkoutRequest.CaloriesBurned != nil {
+		existingWorkout.CaloriesBurned = *updateWorkoutRequest.CaloriesBurned
+	}
+	if updateWorkoutRequest.Entries != nil {
+		existingWorkout.Entries = updateWorkoutRequest.Entries
+	}
+
+	/* currentUser := middleware.GetUser(r)
+	if currentUser == nil || currentUser == store.AnonymousUser {
+		utils.WriteJSON(w, http.StatusBadRequest, utils.Envelope{"error": "you must be logged in to update"})
+		return
+	}
+	*/
+	/* 	workoutOwner, err := wh.workoutStore.GetWorkoutOwner(workoutID)
+	   	if err != nil {
+	   		if errors.Is(err, sql.ErrNoRows) {
+	   			utils.WriteJSON(w, http.StatusNotFound, utils.Envelope{"error": "workout does not exist"})
+	   			return
+	   		}
+
+	   		utils.WriteJSON(w, http.StatusInternalServerError, utils.Envelope{"error": "internal server error"})
+	   		return
+	   	}
+
+	   	if workoutOwner != currentUser.ID {
+	   		//utils.WriteJSON(w, http.StatusForbidden, utils.Envelope{"error": "you are not authorized to update this workout"})
+	   		return
+	   	} */
+
+	err = wh.workoutStore.UpdateWorkout(existingWorkout)
+	if err != nil {
+		wh.logger.Printf("ERROR: updatingWorkout: %v", err)
+		//utils.WriteJSON(w, http.StatusInternalServerError, utils.Envelope{"error": "internal server error"})
+		http.Error(w, "failed to update workout", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(existingWorkout)
+	//utils.WriteJSON(w, http.StatusOK, utils.Envelope{"workout": existingWorkout})
+
 }
