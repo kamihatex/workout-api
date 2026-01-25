a5dc12594be23e659c4519e187ef5ee077810176
kamihatex <plutoismycousin@gmail.com>
2026-01-25 21:29:35 +0200

    helper func to connect test db

---
+++ internal/store/wokrout_store_test.go
@@ -0,0 +1,28 @@
+package store
+
+import (
+	"database/sql"
+	"testing"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+func setupTestDB(t *testing.T) *sql.DB {
+	db, err := sql.Open("pgx", "host=localhost user=postgres password=postgres dbname=postgres port=5433 sslmode=disable")
+	if err != nil {
+		t.Fatalf("opening test db: %v", err)
+	}
+
+	// run the migratoins for our test db
+	err = Migrate(db, "../../migrations/")
+	if err != nil {
+		t.Fatalf("migrating test db error: %v", err)
+	}
+
+	_, err = db.Exec(`TRUNCATE users, workouts, workout_entries CASCADE`)
+	if err != nil {
+		t.Fatalf("truncating tables %v", err)
+	}
+
+	return db
+}
